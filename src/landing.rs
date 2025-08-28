CompeyDev/ssh-portfolio

commit 430ba54185f9c6cc362c794f73f03304c60cc202

feat: top-level panic recovery mechanism

src/main.rs
@@ -51,10 +51,27 @@ async fn main() -> Result<()> {
     let ssh_socket_addr = SSH_SOCKET_ADDR.ok_or(eyre!("Invalid host IP provided"))?;
     let web_server_addr = WEB_SERVER_ADDR.ok_or(eyre!("Invalid host IP provided"))?;
 
-    let ssh_config = tokio::task::block_in_place(ssh_config);
-    tokio::select! {
-        ssh_res = SshServer::start(ssh_socket_addr, ssh_config) => ssh_res,
-        web_res = WebLandingServer::start(web_server_addr) => web_res.map_err(|err| eyre!(err)),
+    let local_set = tokio::task::LocalSet::new();
+    loop {
+        let task = local_set.run_until(async { 
+            tokio::task::spawn_local(async move {
+                let ssh_config = ssh_config();
+                tokio::select! {
+                    ssh_res = SshServer::start(ssh_socket_addr, ssh_config) => ssh_res,
+                    web_res = WebLandingServer::start(web_server_addr) => web_res.map_err(|err| eyre!(err))
+                }
+            }).await
+        });
+
+        match task.await {
+            Ok(res) => break res,
+            Err(err) => {
+                if err.is_panic() {
+                    tracing::error!("Application panicked, recovering...");
+                    std::panic::resume_unwind(err.into_panic());
+                }
+            }
+        }
     }
 }
 

commit 84bb3a89595983e0db083bfdabcec129ed006e48

feat(landing): use `axum` instead of `actix`
Fixes panic on startup due to `LocalSet` usage which was only used due
to `actix` having very complicated internal types which were often `!Send`.

This bug was introduced in commit `430ba54`. Reliance on `LocalSet` has
been removed altogether by migrating to `axum` instead.

Cargo.toml
@@ -50,13 +50,13 @@ crates = ["ratatui-image"]
 ratatui-image = { path = "./target/patch/ratatui-image-8.0.1" }
 
 [dependencies]
-actix-web = "4.11.0"
 anyhow = "1.0.90"
 async-trait = "0.1.85"
 atrium-api = { version = "0.25.4", optional = true }
 atrium-common = { version = "0.1.2", optional = true }
 atrium-xrpc = { version = "0.12.3", optional = true }
 atrium-xrpc-client = { version = "0.5.14", optional = true, default-features = false, features = ["reqwest"] }
+axum = "0.8.4"
 better-panic = "0.3.0"
 bstr = "1.11.3"
 chrono = { version = "0.4.41", optional = true }
@@ -89,7 +89,7 @@ ratatui = { version = "0.29.0", features = ["serde", "macros"] }
 ratatui-image = { version = "8.0.1", optional = true }
 reqwest = { version = "0.12", optional = true, default-features = false, features = ["charset", "http2", "rustls-tls"] }
 russh = "0.49.2"
-rust-embed = { version = "8.7.2", features = ["actix"] }
+rust-embed = { version = "8.7.2" }
 serde = { version = "1.0.211", features = ["derive"] }
 serde_json = "1.0.132"
 ssh-key = { version = "0.6.7", features = ["getrandom", "crypto"] }
@@ -98,6 +98,7 @@ strip-ansi-escapes = "0.2.0"
 strum = { version = "0.26.3", features = ["derive"] }
 tokio = { version = "1.40.0", features = ["full"] }
 tokio-util = "0.7.12"
+tower-http = { version = "0.6.6", features = ["trace"] }
 tracing = "0.1.40"
 tracing-error = "0.2.0"
 tracing-subscriber = { version = "0.3.18", features = ["env-filter", "serde"] }

src/landing.rs
@@ -1,35 +1,15 @@
 use std::io;
 use std::net::SocketAddr;
 
-use actix_web::middleware::Logger;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use axum::extract::Path;
+use axum::http::{header, StatusCode};
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::Router;
 use rust_embed::Embed;
+use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
 use tracing::instrument;
-
-macro_rules! embedded_route {
-    ($path:expr) => {
-        match WebLandingServer::get($path) {
-            Some(content) => HttpResponse::Ok()
-                .content_type(mime_guess::from_path($path).first_or_octet_stream().as_ref())
-                .body(content.data.into_owned()),
-            None => HttpResponse::NotFound().body("404 Not Found"),
-        }
-    };
-
-    ($name:ident,$pat:expr,$path:literal) => {
-        #[actix_web::get($pat)]
-        async fn $name() -> impl Responder {
-            embedded_route!($path)
-        }
-    };
-
-    ($name:ident,$pat:expr) => {
-        #[actix_web::get($pat)]
-        async fn $name(path: web::Path<String>) -> impl Responder {
-            embedded_route!(path.as_str())
-        }
-    };
-}
 
 #[derive(Embed)]
 #[folder = "www/build"]
@@ -38,17 +18,28 @@ pub struct WebLandingServer;
 impl WebLandingServer {
     #[instrument(level = "trace")]
     pub async fn start(addr: SocketAddr) -> io::Result<()> {
+        let app = Router::new()
+            .route("/", get(handle_index))
+            .route("/{*path}", get(handle_static_file))
+            .layer(TraceLayer::new_for_http());
+
+        let listener = TcpListener::bind(addr).await?;
         tracing::info!("Web server listening on {}", addr);
-        HttpServer::new(|| {
-            // TODO: register a default service for a nicer 404 page
-            App::new().service(index).service(favicon).service(dist).wrap(Logger::default())
-        })
-        .bind(addr)?
-        .run()
-        .await
+
+        axum::serve(listener, app).await?;
+        Ok(())
     }
 }
 
-embedded_route!(index, "/", "index.html");
-embedded_route!(favicon, "/favicon.png", "favicon.png");
-embedded_route!(dist, "/{path:.*}");
+async fn handle_index() -> impl IntoResponse {
+    handle_static_file(Path("index.html".into())).await
+}
+
+async fn handle_static_file(Path(path): Path<String>) -> impl IntoResponse {
+    WebLandingServer::get(&path)
+        .map(|file| {
+            let mime = mime_guess::from_path(&path).first_or_octet_stream();
+            ([(header::CONTENT_TYPE, mime.to_string())], file.data)
+        })
+        .ok_or((StatusCode::NOT_FOUND, "404 Not Found"))
+}

src/main.rs
@@ -51,16 +51,13 @@ async fn main() -> Result<()> {
     let ssh_socket_addr = SSH_SOCKET_ADDR.ok_or(eyre!("Invalid host IP provided"))?;
     let web_server_addr = WEB_SERVER_ADDR.ok_or(eyre!("Invalid host IP provided"))?;
 
-    let local_set = tokio::task::LocalSet::new();
     loop {
-        let task = local_set.run_until(async { 
-            tokio::task::spawn_local(async move {
-                let ssh_config = ssh_config();
-                tokio::select! {
-                    ssh_res = SshServer::start(ssh_socket_addr, ssh_config) => ssh_res,
-                    web_res = WebLandingServer::start(web_server_addr) => web_res.map_err(|err| eyre!(err))
-                }
-            }).await
+        let task = tokio::task::spawn(async move {
+            let ssh_config = ssh_config();
+            tokio::select! {
+                ssh_res = SshServer::start(ssh_socket_addr, ssh_config) => ssh_res,
+                web_res = WebLandingServer::start(web_server_addr) => web_res.map_err(|err| eyre!(err))
+            }
         });
 
         match task.await {