a5dfeb739c7777ddbb64377cee1452c5bdeeec0e
Erica Marigold <hi@devcomp.xyz>
2025-08-28 11:35:22 +0100

    feat(www): update website to show `erica` as username :3

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	import { onMount } from 'svelte';
 	import { crossfade, fade } from 'svelte/transition';
 
-	const command = 'ssh -o SendEnv=TERM_PROGRAM devcomp.xyz';
+	const command = 'ssh -o SendEnv=TERM_PROGRAM erica@devcomp.xyz';
 	const cursor = '█'
 
 	let showCheckmark = $state(false);
