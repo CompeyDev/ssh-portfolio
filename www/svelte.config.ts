f4ffc7d7e31c6ff16376788c2a364f298b797328
Erica Marigold <hi@devcomp.xyz>
2025-09-03 18:11:18 +0100

    chore(www): use `satisfies` instead of `T{}`

--- www/svelte.config.ts
+++ www/svelte.config.ts
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import type { Config } from '@sveltejs/kit';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default <Config>{
+export default {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
@@ -13,7 +13,7 @@ export default <Config>{
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
 	}
-};
+} satisfies Config;
 
 /* Self-compilation setup: Do not edit! */
 if (process.env.COMPILE_JS === 'true') {
