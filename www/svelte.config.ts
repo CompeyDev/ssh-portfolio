b19e4e025ebb19d5bbbbfedc110096b5b98c27d7
Erica Marigold <hi@devcomp.xyz>
2025-11-15 06:04:04 +0000

    chore(www/pkg): just use typescript config for svelte

--- www/svelte.config.ts
+++ www/svelte.config.ts
@@ -14,17 +14,3 @@ export default {
 		adapter: adapter()
 	}
 } satisfies Config;
-
-/* Self-compilation setup: Do not edit! */
-if (process.env.COMPILE_JS === 'true') {
-	const esbuild = await import('esbuild');
-	esbuild
-		.build({
-			entryPoints: ['svelte.config.ts'],
-			outfile: 'svelte.config.js',
-			minify: true,
-			platform: 'node',
-			banner: { js: '// This is an @generated config. Your changes will be overwritten. Please edit `svelte.config.ts` instead.' }
-		})
-		.catch(() => process.exit(1));
-}
