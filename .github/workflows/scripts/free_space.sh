d656e292703867adb6a3ab104a01ec9b05071428
Erica Marigold <hi@devcomp.xyz>
2025-12-01 06:16:06 +0000

    ci(scripts): use `--apparent-size` to speed up `du`

--- .github/workflows/scripts/free_space.sh
+++ .github/workflows/scripts/free_space.sh
@@ -24,14 +24,14 @@ set -eux
 
 df -h
 echo "::group::/usr/local/*"
-du -hsc /usr/local/*
+du -hsc --apparent-size /usr/local/*
 echo "::endgroup::"
 # ~1GB
 sudo rm -rf \
   /usr/local/aws-sam-cli \
   /usr/local/julia* || :
 echo "::group::/usr/local/bin/*"
-du -hsc /usr/local/bin/*
+du -hsc --apparent-size /usr/local/bin/*
 echo "::endgroup::"
 # ~1GB (From 1.2GB to 214MB)
 sudo rm -rf \
@@ -53,14 +53,14 @@ sudo rm -rf \
 # 142M
 sudo rm -rf /usr/local/bin/oc || : \
 echo "::group::/usr/local/share/*"
-du -hsc /usr/local/share/*
+du -hsc --apparent-size /usr/local/share/*
 echo "::endgroup::"
 # 506MB
 sudo rm -rf /usr/local/share/chromium || :
 # 1.3GB
 sudo rm -rf /usr/local/share/powershell || :
 echo "::group::/usr/local/lib/*"
-du -hsc /usr/local/lib/*
+du -hsc --apparent-size /usr/local/lib/*
 echo "::endgroup::"
 # 15GB
 sudo rm -rf /usr/local/lib/android || :
@@ -69,17 +69,17 @@ sudo rm -rf /usr/local/lib/heroku || :
 # 1.2GB
 sudo rm -rf /usr/local/lib/node_modules || :
 echo "::group::/opt/*"
-du -hsc /opt/*
+du -hsc --apparent-size /opt/*
 echo "::endgroup::"
 # 679MB
 sudo rm -rf /opt/az || :
 echo "::group::/opt/microsoft/*"
-du -hsc /opt/microsoft/*
+du -hsc --apparent-size /opt/microsoft/*
 echo "::endgroup::"
 # 197MB
 sudo rm -rf /opt/microsoft/powershell || :
 echo "::group::/opt/hostedtoolcache/*"
-du -hsc /opt/hostedtoolcache/*
+du -hsc --apparent-size /opt/hostedtoolcache/*
 echo "::endgroup::"
 # 5.3GB
 sudo rm -rf /opt/hostedtoolcache/CodeQL || :
